caf69612637b7ecfccc8ff78a620374e402a5382
Nivedita Sen <niveditasen14@gmail.com>
2026-06-07 22:12:48 +0530

    Add files via upload

---
+++ Hashing3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hashing3{
+    public static void main(String[] args){
+        HashSet<String> fruits= new HashSet<>();
+        System.out.println("Enter the Number of elements:");
+        int n=sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++){
+            String name=sc.nextLine();
+            set.add(name);
+        }
+        System.out.println(set);
+        sc.close();
+    }
+}
