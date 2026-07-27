5a934c4634bba2b76402ef1a317c4ea5a42e42b0
Nivedita Sen <niveditasen14@gmail.com>
2026-07-27 22:39:21 +0530

    Add optimal left rotation algorithm for array

---
+++ left_rotate_the_arr_by_D_place_optimal.java
@@ -0,0 +1,24 @@
+class left_rotate_the_arr_by_D_place_optimal {
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int d = 3;
+        int n = arr.length;
+        d = d % n;
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+        System.out.print("Rotated Array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
